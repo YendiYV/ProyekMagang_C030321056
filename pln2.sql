64a456c76bd656440efdcc792559681536ad4adc
YendiYV <yendiyv3903@gmail.com>
2023-10-02 13:47:42 +0800

    2 Okt 13.47

--- pln2.sql
+++ pln2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Sep 2023 pada 03.46
+-- Waktu pembuatan: 02 Okt 2023 pada 06.08
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 8.1.6
 
@@ -45,7 +45,10 @@ CREATE TABLE `cuti` (
 --
 
 INSERT INTO `cuti` (`id_cuti`, `id_user`, `alasan`, `tgl_diajukan`, `mulai`, `berakhir`, `id_status_cuti1`, `id_status_cuti2`, `id_status_cuti3`, `perihal_cuti`) VALUES
-('cuti-66080', 'c551fc8847d29dc25a23db5d2cdb941b', 'Tau', '2023-09-21', '2023-09-21', '2023-09-27', 2, 2, 3, 'Coba Coba');
+('4533-SP-Cuti-2023', 'dce802a5e29e9ccabc144dfb6a37abbb', 'tes', '2023-10-02', '2023-10-03', '2023-10-11', 2, 2, 2, 'Hndk banar ai'),
+('8103-SP-Cuti-2023', 'dce802a5e29e9ccabc144dfb6a37abbb', 'tse', '2023-10-02', '2023-10-27', '2023-10-28', 1, 1, 1, 'test'),
+('8880-SP-Cuti-2023', 'c551fc8847d29dc25a23db5d2cdb941b', 'berkunjung RS untuk pengecekan organ dalam', '2023-10-02', '2023-10-03', '2023-10-09', 2, 2, 2, 'Kunjungan RS'),
+('9999-SP-Cuti-2023', 'dce802a5e29e9ccabc144dfb6a37abbb', 'Sakit Jiwa Karena Kerja Tros', '2023-09-29', '2023-09-30', '2023-10-06', 2, 2, 2, 'Sakit');
 
 -- --------------------------------------------------------
 
@@ -94,9 +97,8 @@ INSERT INTO `status_cuti` (`id_status_cuti`, `status_cuti`) VALUES
 
 CREATE TABLE `user` (
   `id_user` varchar(256) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `email` varchar(30) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `id_user_level` int(11) NOT NULL,
   `id_user_detail` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -105,14 +107,13 @@ CREATE TABLE `user` (
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `id_user_level`, `id_user_detail`) VALUES
-('134e349e4f50a051d8ca3687d6a7de1a', 'admin1', '123', 'admin1@gmail.com', 2, '134e349e4f50a051d8ca3687d6a7de1a'),
-('98eb4077470a60a0fe0f7b9d01755557', 'admin', '123', 'admin@gmail.com', 2, '98eb4077470a60a0fe0f7b9d01755557'),
-('c551fc8847d29dc25a23db5d2cdb941b', 'rian', '123', 'rian@gmail.com', 1, 'c551fc8847d29dc25a23db5d2cdb941b'),
-('dce802a5e29e9ccabc144dfb6a37abbb', 'ika', '123', 'ika@gmail.com', 1, 'dce802a5e29e9ccabc144dfb6a37abbb'),
-('eb71208764d1a8a02cdf86a49ccd1489', 'Manajer', '123', 'manajer@gmail.com', 4, 'eb71208764d1a8a02cdf86a49ccd1489'),
-('f5972fbf4ef53843c1e12c3ae99e5005', 'supervisior', 'tes123', 'supervisior@gmail.com', 3, 'f5972fbf4ef53843c1e12c3ae99e5005'),
-('f7c7b7e19a4ed7a51db593c8efbee984', 'Aminudin', '123', 'aminudin@gmail.com', 1, 'f7c7b7e19a4ed7a51db593c8efbee984');
+INSERT INTO `user` (`id_user`, `username`, `password`, `id_user_level`, `id_user_detail`) VALUES
+('134e349e4f50a051d8ca3687d6a7de1a', '9014050BJM', '202cb962ac59075b964b07152d234b70', 2, '134e349e4f50a051d8ca3687d6a7de1a'),
+('c551fc8847d29dc25a23db5d2cdb941b', '209090PKY', '202cb962ac59075b964b07152d234b70', 1, 'c551fc8847d29dc25a23db5d2cdb941b'),
+('dce802a5e29e9ccabc144dfb6a37abbb', '209091PKY', '202cb962ac59075b964b07152d234b70', 1, 'dce802a5e29e9ccabc144dfb6a37abbb'),
+('eb71208764d1a8a02cdf86a49ccd1489', 'manajer', '202cb962ac59075b964b07152d234b70', 4, 'eb71208764d1a8a02cdf86a49ccd1489'),
+('f5972fbf4ef53843c1e12c3ae99e5005', 'supervisio', '202cb962ac59075b964b07152d234b70', 3, 'f5972fbf4ef53843c1e12c3ae99e5005'),
+('f7c7b7e19a4ed7a51db593c8efbee984', '209092PKY', '827ccb0eea8a706c4c34a16891f84e7b', 1, 'f7c7b7e19a4ed7a51db593c8efbee984');
 
 -- --------------------------------------------------------
 
@@ -127,7 +128,7 @@ CREATE TABLE `user_detail` (
   `no_telp` varchar(30) DEFAULT NULL,
   `alamat` text DEFAULT NULL,
   `nip` varchar(10) DEFAULT NULL,
-  `pangkat` varchar(50) DEFAULT NULL,
+  `proyek` varchar(50) DEFAULT NULL,
   `jabatan` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -135,14 +136,13 @@ CREATE TABLE `user_detail` (
 -- Dumping data untuk tabel `user_detail`
 --
 
-INSERT INTO `user_detail` (`id_user_detail`, `nama_lengkap`, `id_jenis_kelamin`, `no_telp`, `alamat`, `nip`, `pangkat`, `jabatan`) VALUES
-('134e349e4f50a051d8ca3687d6a7de1a', 'Admin1', NULL, NULL, NULL, NULL, NULL, 'Admin'),
-('98eb4077470a60a0fe0f7b9d01755557', 'Admin', 1, NULL, NULL, NULL, NULL, 'Admin'),
-('c551fc8847d29dc25a23db5d2cdb941b', 'Operator Rian', 2, '+62812781728', 'Jl. Sekip', '2019220028', 'Golo 1', 'Operator'),
-('dce802a5e29e9ccabc144dfb6a37abbb', 'Operator Ika', 2, '+62812781728', 'Jl. Sekip', '1111', 'Golo 1', 'Operator'),
+INSERT INTO `user_detail` (`id_user_detail`, `nama_lengkap`, `id_jenis_kelamin`, `no_telp`, `alamat`, `nip`, `proyek`, `jabatan`) VALUES
+('134e349e4f50a051d8ca3687d6a7de1a', 'Admin', 1, '08080808', 'Jl. Pangeran H No.22', '9014050BJM', NULL, 'Admin'),
+('c551fc8847d29dc25a23db5d2cdb941b', 'Rian Ahmad', 1, '+62812781728', 'Jl. Sekip', '209090PKY', 'Proyek PKY', 'Operator 2'),
+('dce802a5e29e9ccabc144dfb6a37abbb', 'Ika Septiana', 2, '+62812781728', 'Jl. Sekip', '209091PKY', 'Proyek PP', 'Operator 2'),
 ('eb71208764d1a8a02cdf86a49ccd1489', 'Manajer Yendi', 1, '081212121212', 'Jl. Hidayatullah No.22', NULL, NULL, 'Manajer'),
 ('f5972fbf4ef53843c1e12c3ae99e5005', 'Supervisior', 1, NULL, NULL, NULL, NULL, 'Supervisior'),
-('f7c7b7e19a4ed7a51db593c8efbee984', 'Operator Aminudin', 1, '+62812781728', 'Jl. Sekip', '2019220028', 'Gol 1', 'Bos');
+('f7c7b7e19a4ed7a51db593c8efbee984', 'Operator Aminudin', 1, '+62812781728123', 'Jl. Sekip', '209092PKY', 'Proyek TL', 'Operator 1');
 
 -- --------------------------------------------------------
 
